a64a04883156ec7b1afca4b83d287f17a8e34dcb
Angel-H-ernandez <104273138+Angel-H-ernandez@users.noreply.github.com>
2024-10-29 20:07:49 -0600

    Update User_administrador_controller.php importTrabajador model directo en github

--- app/Http/Controllers/User_administrador_controller.php
+++ app/Http/Controllers/User_administrador_controller.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User_administrador_model;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+use App\Models\Trabajador_model;
+use App\Models\Users_model;
 
 class User_administrador_controller extends Controller
 {
